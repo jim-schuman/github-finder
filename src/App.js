c38207c98f62190d601479a9e4366c9cb142baa5
jim schuman <jim@jims-iMac.local>
2021-01-17 15:43:31 -0600

    finish point - pre add ing context and functional components

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     loading: false,
     searchUsers: [],
     clearUsers: [],
-    alert: null
+    alert: null,
+    repos: []
   }
 
   static propTypes = {
@@ -50,6 +51,14 @@ class App extends Component {
     this.setState({user: res.data, loading: false});
   }
 
+  // get users repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({repos: res.data, loading: false});
+  }
+
   // clear users
   clearUsers = () => this.setState({users:[], loading: false});
 
@@ -61,7 +70,7 @@ class App extends Component {
 
   render() {
 
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -86,7 +95,9 @@ class App extends Component {
                   <User
                       { ...props }
                       getUser={this.getUser}
+                      getUserRepos={this.getUserRepos}
                       user={user}
+                      repos={repos}
                       loading={loading}
                   />
               )} />
