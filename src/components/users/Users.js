e89a45c3bbf64b6067786c01b0622963e0ca099c
jim schuman <jim@jims-iMac.local>
2021-01-18 14:38:47 -0600

    alert using context

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,6 @@
 import React, {useContext} from 'react'
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
 
 const Users = () => {
@@ -22,8 +21,6 @@ const Users = () => {
     }
     
 }
-
-
 
 const userStyle = {
     display: 'grid',
